f98fc32a011def0147192b3fe70bff78c0ebd3d6
Blacky Wolf <support@blackywolf.me>
2022-07-19 23:57:20 -0500

    Finished setting up settings model for Fauna

--- app/data/db-providers/fauna/FaunaDb.ts
+++ app/data/db-providers/fauna/FaunaDb.ts
@@ -1,10 +1,22 @@
+import { Client } from "$fauna";
 import { Database, SettingsEntity } from "@data/abstractions/index.ts";
 import { FaunaSettingsEntity } from "./entities/FaunaSettingsEntity.ts";
 
 export class FaunaDb implements Database {
+    private client: Client;
+
     settings: SettingsEntity;
 
     constructor() {
-        this.settings = new FaunaSettingsEntity();
+        const secret = Deno.env.get("FAUNA_SECRET");
+
+        if (!secret) throw new Error("The FAUNA_SECRET environment variable is required.");
+
+        this.client = new Client({
+            domain: Deno.env.get("FAUNA_ENDPOINT"),
+            secret: secret
+        });
+
+        this.settings = new FaunaSettingsEntity(this.client);
     }
 }
--- app/data/db-providers/fauna/entities/FaunaSettingsEntity.ts
+++ app/data/db-providers/fauna/entities/FaunaSettingsEntity.ts
@@ -1,25 +1,75 @@
-import {  } from "$fauna";
+import { Client, query, values } from "$fauna";
 import { SettingsEntity } from "@data/abstractions/index.ts";
 import { Settings } from "@data/models/Settings.ts";
 
+type SettingsDocument = values.Document<Settings>;
+type SettingsPage = values.Page<SettingsDocument>;
+
 export class FaunaSettingsEntity implements SettingsEntity {
-    add(settings: Settings): void {
-        throw new Error("Method not implemented.");
+    private collection = "settings";
+
+    constructor(private client: Client) {}
+
+    async add(settings: Settings) {
+        const document = await this.client.query<SettingsDocument>(
+            query.Create(
+                query.Collection(this.collection),
+                { data: settings }
+            )
+        );
+
+        settings.id = document.ref.id;
     }
 
-    getAll(): Settings[] {
-        throw new Error("Method not implemented.");
+    async getAll() {
+        const documents = await this.client.query<SettingsPage>(
+            query.Map(
+                query.Paginate(
+                    query.Documents(
+                        query.Collection(this.collection)
+                    )
+                ),
+                query.Lambda(
+                    'ref',
+                    query.Get(
+                        query.Var('ref')
+                    )
+                )
+            )
+        );
+
+        return documents.data.map(document => ({ ...document.data, id: document.ref.id }));
     }
 
-    getById(id: string): Settings {
-        throw new Error("Method not implemented.");
+    async getById(id: string) {
+        const document = await this.client.query<SettingsDocument>(
+            query.Get(
+                query.Ref(
+                    query.Collection(this.collection),
+                    id
+                )
+            )
+        );
+
+        return { ...document.data, id: document.ref.id };
     }
 
-    remove(id: string): void {
-        throw new Error("Method not implemented.");
+    async remove(id: string) {
+        await this.client.query<SettingsDocument>(
+            query.Delete(
+                query.Ref(query.Collection(this.collection), id)
+            )
+        );
     }
 
-    update(settings: Settings): void {
-        throw new Error("Method not implemented.");
+    async update(settings: Settings) {
+        const { id, ...data } = settings;
+
+        await this.client.query<SettingsDocument>(
+            query.Update(
+                query.Ref(query.Collection(this.collection), id),
+                { data }
+            )
+        );
     }
 }
