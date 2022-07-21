ad553460f4d0effc6589669162588f5c5e7eaab1
Blacky Wolf <support@blackywolf.me>
2022-07-20 23:00:20 -0500

    Added deno env; added settings loading

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,12 @@
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
 
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { InnerRenderFunction, RenderContext, start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
+import { setupSettings } from "@utilities/site.ts";
+
+await setupSettings();
 
 function render(context: RenderContext, render: InnerRenderFunction) {
     render();
