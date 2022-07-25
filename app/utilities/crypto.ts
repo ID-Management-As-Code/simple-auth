3dfa8c94474dc4b9ce3d0be2d03ad832dcb57d9b
Blacky Wolf <support@blackywolf.me>
2022-07-24 22:34:12 -0500

    Added utilities for working with cryptography

---
+++ app/utilities/crypto.ts
@@ -0,0 +1,18 @@
+import { argon2id, argon2Verify } from "$argon";
+import { crypto } from "$crypto";
+import { EnvKey, getEnvValue } from "./env.ts";
+
+export const hashPassword = async (password: string) => await argon2id({
+    password,
+    hashLength: getEnvValue<number>(EnvKey.ARGON_HASH_LENGTH) || 256,
+    iterations: getEnvValue<number>(EnvKey.ARGON_ITERATIONS) || 256,
+    memorySize: getEnvValue<number>(EnvKey.ARGON_MEMORY_SIZE) || 4096,
+    outputType: "encoded",
+    parallelism: getEnvValue<number>(EnvKey.ARGON_PARALLELISM) || 4,
+    salt: crypto.getRandomValues(new Uint16Array(32)),
+});
+
+export const verifyPassword = async (password: string, hash: string) => await argon2Verify({
+    password,
+    hash
+});
