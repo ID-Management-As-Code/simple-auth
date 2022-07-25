35a072524fd4cdc432bb3fd24b750e0d47103038
Blacky Wolf <support@blackywolf.me>
2022-07-24 22:38:25 -0500

    Ignored request handler for middleware

--- app/routes/_middleware.ts
+++ app/routes/_middleware.ts
@@ -1,7 +1,7 @@
 import { MiddlewareHandlerContext } from "$fresh/server.ts";
 import { settings } from "@utilities/site.ts";
 
-export const handler = async (request: Request, context: MiddlewareHandlerContext) => {
+export const handler = async (_: Request, context: MiddlewareHandlerContext) => {
 
     if (settings?.underMaintenance) {
         return new Response("Under maintenance, come back later.");
